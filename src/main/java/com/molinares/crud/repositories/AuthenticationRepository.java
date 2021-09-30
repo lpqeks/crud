3b64b1794185986be3ebf9e7734a6357a40f7515
Carlos Molinares <carlos.molinares@austincc.edu>
2021-09-30 09:26:56 -0500

    ffinal version

--- src/main/java/com/molinares/crud/repositories/AuthenticationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.molinares.crud.repositories;
-
-import com.molinares.crud.models.Authentication;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthenticationRepository extends CrudRepository<Authentication, Long> {
-}
